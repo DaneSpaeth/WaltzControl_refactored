439da4c835235da4c3dc6f390fb5ba7a3c13594c
Your Name Here <dane.spaeth@gmail.com>
2018-10-04 11:27:45 +0200

    Add LX200 move and stop commands and tests

--- WaltzControl/external_interfaces/lx200_commands.py
+++ WaltzControl/external_interfaces/lx200_commands.py
@@ -10,4 +10,52 @@ def get_telescope_dec():
     """Return Lx200 protocol command for "Get Telescope Declination".
     """
     command = b'#:GD#'
-    return command 
+    return command
+
+def move_telescope_west():
+    """Return Lx200 protocol command for "move_telescope_west".
+    """
+    command = b'#:Mw#'
+    return command
+
+def move_telescope_east():
+    """Return Lx200 protocol command for "move_telescope_east".
+    """
+    command = b'#:Me#'
+    return command
+
+def move_telescope_north():
+    """Return Lx200 protocol command for "move_telescope_north".
+    """
+    command = b'#:Mn#'
+    return command
+
+def move_telescope_south():
+    """Return Lx200 protocol command for "move_telescope_south".
+    """
+    command = b'#:Ms#'
+    return command
+
+def stop_telescope_west():
+    """Return Lx200 protocol command for "halt westward slew".
+    """
+    command = b'#:Qw#'
+    return command
+
+def stop_telescope_east():
+    """Return Lx200 protocol command for "halt eastward slew".
+    """
+    command = b'#:Qe#'
+    return command
+
+def stop_telescope_north():
+    """Return Lx200 protocol command for "halt northward slew".
+    """
+    command = b'#:Qn#'
+    return command
+
+def stop_telescope_south():
+    """Return Lx200 protocol command for "halt southward slew".
+    """
+    command = b'#:Qs#'
+    return command
--- WaltzControl/external_interfaces/tests/test_lx200_commands.py
+++ WaltzControl/external_interfaces/tests/test_lx200_commands.py
@@ -9,3 +9,45 @@ def test_get_telescope_dec():
     """Test get_telescope_dec."""
     command = lx.get_telescope_dec()
     assert command == b'#:GD#'
+    
+def test_move_telescope_west():
+    """Test test_move_telescope_west."""
+    command = lx.move_telescope_west()
+    assert command == b'#:Mw#'
+    
+def test_move_telescope_east():
+    """Test move_telescope_east."""
+    command = lx.move_telescope_east()
+    assert command == b'#:Me#'
+    
+def test_move_telescope_north():
+    """Test move_telescope_north."""
+    command = lx.move_telescope_north()
+    assert command == b'#:Mn#'
+    
+def test_move_telescope_south():
+    """Test move_telescope_south."""
+    command = lx.move_telescope_south()
+    assert command == b'#:Ms#'
+    
+def test_stop_telescope_west():
+    """Test stop_telescope_west."""
+    command = lx.stop_telescope_west()
+    assert command == b'#:Qw#'
+    
+def test_stop_telescope_east():
+    """Test stop_telescope_east."""
+    command = lx.stop_telescope_east()
+    assert command == b'#:Qe#'
+    
+def test_stop_telescope_north():
+    """Test stop_telescope_north."""
+    command = lx.stop_telescope_north()
+    assert command == b'#:Qn#'
+    
+def test_stop_telescope_south():
+    """Test stop_telescope_south."""
+    command = lx.stop_telescope_south()
+    assert command == b'#:Qs#'
+    
+
